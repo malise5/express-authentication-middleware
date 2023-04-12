69f5ec7f7aa55c385c8e58f690daaa6d337eb9a6
Halkano Malise <kudezx@gmail.com>
2023-04-12 16:16:13 +0300

    updated

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const port = 8000;
 const userRoutes = require("./src/routes/userRoutes");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/v1/users", userRoutes);
 
