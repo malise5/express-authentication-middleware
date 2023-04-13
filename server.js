58c93a6b4e833c038437d16e30c185d23b7cb3d0
Halkano Malise <kudezx@gmail.com>
2023-04-13 15:03:29 +0300

    connect to remote mongoDb

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
@@ -5,9 +6,25 @@ const app = require("./app");
 const port = process.env.PORT || 8000;
 
 // console.log(process.env);
+
+//CONNECT TO DB
+mongoose
+    .connect(process.env.CONN_DB, {
+        useNewUrlParser: true,
+    })
+    .then((con) => {
+        // console.log(con);
+        console.log("=============DataBase=======================");
+        console.log("DB onnection successful");
+        console.log("====================================");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
 //SERVER
 app.listen(port, () => {
-    console.log("====================================");
+    console.log("=============Server=======================");
     console.log(`SERVER RUNNING ON PORT ${port}......`);
     console.log("====================================");
 });
