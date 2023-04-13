3d7072e5e06acfdd6fd614f0f7c93c7ce9d466dc
Halkano Malise <kudezx@gmail.com>
2023-04-13 16:09:41 +0300

    updated

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers } = require("./../controllers/userController");
+const {
+    getAllUsers,
+    createUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+} = require("./../controllers/userController");
 
-//params middleware
-// router.param("id", (req, res, next, value) => {
-//   console.log("user id" + value);
-//   next();
-// });
-
-router.route("/").get(getAllUsers);
-// .post(createUsers);
-// router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+router.route("/").get(getAllUsers).post(createUsers);
+router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
