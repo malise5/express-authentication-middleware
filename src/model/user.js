0352eb0278cf4acb9fe5232e38198106060f3da4
Halkano Malise <kudezx@gmail.com>
2023-04-13 15:17:07 +0300

    models updated

--- src/model/user.js
+++ src/model/user.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const User = mongoose.model('User', {
-    name: String,
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "name must filled "],
+    },
     email: String,
-})
+    age: Number,
+    gender: String,
+});
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
